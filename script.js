80f25447c7441f97160cab7860323d273cbddf09
annacallahan <anna.r.callahan@gmail.com>
2020-03-22 15:53:15 -0500

    finished index.html and script.js code

--- script.js
+++ script.js
@@ -1,4 +1,23 @@
 // Write your JavaScript code here!
+window.addEventListener("load", function(){
+   fetch("https://handlers.education.launchcode.org/static/planets.json").then(function(response){
+    return response.json()
+   }).then(function(json){
+      const missionTarget = document.getElementById("missionTarget");
+      let index = Math.floor(Math.random()*6);
+         missionTarget.innerHTML = `
+         <h2>Mission Destination</h2>
+         <ol>
+            <li>Name: ${json[index].name}</li>
+            <li>Diameter: ${json[index].diameter}</li>
+            <li>Star: ${json[index].star}</li>
+            <li>Distance from Earth: ${json[index].distance}</li>
+            <li>Number of Moons: ${json[index].moons}</li>
+         </ol>
+         <img src= ${json[index].image}>
+         `
+   });
+});
 
 /* This block of code shows how to format the HTML once you fetch some planetary JSON!
 <h2>Mission Destination</h2>
